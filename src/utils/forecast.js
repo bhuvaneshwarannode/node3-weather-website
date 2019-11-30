3f6f773296bf228f7becf89a54c8cda7068e8988
bhuvaneshwaran <bhuvaneshwaran8996@gmail.com>
2019-11-30 22:12:32 +0530

    initial push

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'https://api.darksky.net/forecast/36841f8c8551ca523f6875f49adc7ea5/' + latitude + ',' + longitude
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
